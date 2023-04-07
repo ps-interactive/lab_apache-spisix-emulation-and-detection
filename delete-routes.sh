97f81eac3f4be0fdfebbe4953c832989f5e321a2
West <wshepherd0010@gmail.com>
2023-04-06 21:04:05 -0400

    Create delete-routes.sh

---
+++ delete-routes.sh
@@ -0,0 +1,19 @@
+curl -s -X 'POST' http://172.31.24.111/apisix/batch-requests -o- \
+ -H 'Host: 127.0.0.1:9080' \
+ -H 'X-API-KEY: edd1c9f034335f136f87ad84b625c8f1' \
+ -H 'Accept: */*' \
+ -H 'Accept-Encoding: utf8' \
+ -H 'Content-Type: application/json' \
+ -H 'Connection: close' \
+--data "$(echo -n "{
+ \"headers\": {
+  \"X-Real-IP\": \"127.0.0.1\",
+  \"X-API-KEY\": \"edd1c9f034335f136f87ad84b625c8f1\",
+  \"Content-Type\": \"application/json\"
+ },
+ \"timeout\": 1500,
+ \"pipeline\": [{
+  \"path\": \"/apisix/admin/routes/index\",
+  \"method\": \"DELETE\"
+ }]
+}")"|jq
