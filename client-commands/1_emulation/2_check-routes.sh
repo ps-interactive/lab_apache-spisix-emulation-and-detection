3413298a11d05cc5a56fde7d46843cf8a5b66a89
West Shepherd <wshepherd0010@gmail.com>
2023-04-16 10:02:36 -0400

    fixed api key and job issue

--- client-commands/1_emulation/2_check-routes.sh
+++ client-commands/1_emulation/2_check-routes.sh
@@ -1,2 +1,2 @@
 curl -G -s 'http://172.31.24.111/apisix/admin/routes' \
---data-urlencode 'api_key=EDD1C9F034335F136F87AD84B625C8F1'|jq
+--data-urlencode 'api_key=edd1c9f034335f136f87ad84b625c8f1'|jq
